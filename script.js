31bd883a7387306709a26d825cf4f56ed4c2d53b
AngelinaFruehwirth <angelina.fruehwirth@stud.fhgr.ch>
2025-10-13 11:37:16 +0200

    datepicker

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function getAll() {
+/*async function getAll() {
     const url = 'https://im3.angelina-fruehwirth.ch/backend/api/getAll.php';
     try {
     const response = await fetch(url);
@@ -10,4 +10,26 @@ async function getAll() {
 
 }
 
-getAll(); 
+getAll(); */
+
+
+async function getByDate(date){
+    const url = `https://im3.angelina-fruehwirth.ch/backend/api/getByDate.php?date=${date}`;
+    try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data); // gibt die Daten der API in der Konsole aus
+    } catch (error) {
+    console.error(error)
+    }
+
+}
+
+const datepicker = document.querySelector('#datepicker');
+datepicker.addEventListener('change', function(){
+    const date = datepicker.value;
+    getByDate(date)
+    console.log(date);
+
+
+})
